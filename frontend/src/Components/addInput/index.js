e37d3fceade3f9543027231c2ca629d6fd3a1fa6
harpreet-singh-au7 <simransingh12340@gmail.com>
2020-11-16 18:08:19 +0530

    track adding

--- frontend/src/Components/addInput/index.js
+++ frontend/src/Components/addInput/index.js
@@ -1,24 +1,28 @@
-import { Collapse, Paper, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
-import "./input.css"
-import InputTodo from './inputTodo'
+import { Collapse, Paper, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import "./input.css";
+import InputTodo from "./inputTodo";
 
-function AddInput({listId , type}) {
-    const [open, setOpen] = useState(false)
-    return (
-        <div className="addInput">
-            <Collapse in={open} >
-                <InputTodo setOpen={setOpen} listId={listId} type={type}/>
-            </Collapse>
-            <Collapse in={!open}>
-            <Paper className="addInput_button" elevation={0} onClick={()=>setOpen(!open)} >
-                <Typography className="addInput_text">
-                   {type === "todo"? "Add Todo" :"Add another list"}
-                </Typography>
-                </Paper>
-                </Collapse>
-        </div>
-    )
+function AddInput({ listId, type }) {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="addInput">
+      <Collapse in={open}>
+        <InputTodo setOpen={setOpen} listId={listId} type={type} />
+      </Collapse>
+      <Collapse in={!open}>
+        <Paper
+          className="addInput_button"
+          elevation={0}
+          onClick={() => setOpen(!open)}
+        >
+          <Typography className="addInput_text">
+            {type === "todo" ? "Add Todo" : "Add another list"}
+          </Typography>
+        </Paper>
+      </Collapse>
+    </div>
+  );
 }
 
-export default AddInput
+export default AddInput;
